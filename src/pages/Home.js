78ade8336fe6cd174eefec7fbd48e2abb3460f76
dunleavyjack <dunleavyjack@gmail.com>
2021-03-21 23:55:35 +0900

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 
+const { REACT_APP_CLIENT_ID } = process.env;
+const redirectUrl = "http://localhost:3000/redirect"
+
 const handleLogin = () => {
-    window.location = `http://www.strava.com/oauth/authorize?client_id=46874&response_type=code&redirect_uri=http://localhost:3000/redirect/exchange_token&approval_prompt=force&scope=read`;
+    window.location = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read`;
 };
 
 const Home = () => {
